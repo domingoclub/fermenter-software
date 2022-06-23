48dcfab4985a8df8971cdc3ebb4947d664b27d71
Antoine Jaunard <antoine.stuff@pm.me>
2022-06-23 18:31:57 +0200

    clean old code

--- software/code.py
+++ software/code.py
@@ -1,178 +1,224 @@
 import board
 import busio
 import time
-import displayio
-import terminalio
-import rotaryio
-import pwmio
-import math
 from digitalio import DigitalInOut, Direction, Pull
+import rotaryio
 import adafruit_mcp9808
+import displayio
 import adafruit_displayio_sh1106
 from adafruit_display_text import label
+import terminalio
+import pwmio
+import simpleio
+import math
 import adafruit_rgbled
 
-# Variables
-fan_on = False
-heat_on = False
-temp = 0.0
-temp_set = 30.0
-temp_margin = 1
-timer_set = 0.0
-edit_mode = False
-fan_freq = 0
-fan_freq_max = 6
-current_screen = "temp_display"
+class fermenter:
 
-# Sensor
-sensor_i2c = busio.I2C(board.GP13, board.GP12)
-sensor = adafruit_mcp9808.MCP9808(sensor_i2c)
+    def __init__(self):
 
-# Led
-RED_LED = board.GP22
-GREEN_LED = board.GP21
-BLUE_LED = board.GP20
-led = adafruit_rgbled.RGBLED(RED_LED, GREEN_LED, BLUE_LED, invert_pwm=True)
+        # Time
+        self.TIME_STARTUP = time.time()
+        self.TIME_TIMER_HOURS = 0
+        self.TIMER_ON = False
 
-# Button
-BTN = DigitalInOut(board.GP2)
-BTN.direction = Direction.INPUT
-BTN.pull = Pull.UP
-btn_down = False
+        # Heating System
+        self.STATUS = 0
 
-# Encoder
-encoder = rotaryio.IncrementalEncoder(board.GP4, board.GP3)
-last_position = 0
+        # Settings
+        self.TEMP_SET = 30
+        self.TEMP_MARGIN = 1
+        self.TEMP_MAX = self.TEMP_SET + self.TEMP_MARGIN
+        self.TEMP_MIN = self.TEMP_SET - self.TEMP_MARGIN
+        self.COLOR_RED = (20, 0, 0)
+        self.COLOR_GREEN = (0, 20, 0)
+        self.COLOR_BLUE = (0, 0, 20)
 
-# Display
-WIDTH = 130
-HEIGHT = 64
-displayio.release_displays()
-display_i2c = busio.I2C(board.GP27, board.GP26, frequency=400000)
-display_bus = displayio.I2CDisplay(display_i2c, device_address=0x3c)
-display = adafruit_displayio_sh1106.SH1106(display_bus, width=WIDTH, height=HEIGHT)
+        # Sensor
+        self.sensor_i2c = busio.I2C(board.GP13, board.GP12)
+        self.sensor = adafruit_mcp9808.MCP9808(self.sensor_i2c)
 
-# Display context
-screen = displayio.Group()
-display.show(screen)
-color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
-color_palette = displayio.Palette(1)
-color_palette[0] = 0x000000  # black
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-screen.append(bg_sprite)
+        # Button
+        self.BTN = DigitalInOut(board.GP2)
+        self.BTN.direction = Direction.INPUT
+        self.BTN.pull = Pull.UP
+        self.btn_down = False
 
-# Text
-label_area = label.Label(terminalio.FONT, text=" "*20, color=0xFFFFFF, x=5, y=5)
-value_area = label.Label(terminalio.FONT, text=" "*20, color=0xFFFFFF, x=48, y=32)
-menu_left_area = label.Label(terminalio.FONT, text="<", color=0xFFFFFF, x=5, y=55)
-menu_right_area = label.Label(terminalio.FONT, text=">", color=0xFFFFFF, x=123, y=55)
-screen.append(label_area)
-screen.append(value_area)
-screen.append(menu_left_area)
-screen.append(menu_right_area)
+        # Encoder
+        self.encoder = rotaryio.IncrementalEncoder(board.GP4, board.GP3)
+        self.encoder_last_position = 0
+
+        # Led
+        self.RED_LED = board.GP20
+        self.GREEN_LED = board.GP18
+        self.BLUE_LED = board.GP16
+        self.LED = adafruit_rgbled.RGBLED(self.RED_LED, self.GREEN_LED, self.BLUE_LED, invert_pwm=True)
+
+        # Display
+        displayio.release_displays()
+        display_i2c = busio.I2C(board.GP27, board.GP26, frequency=400000)
+        display_bus = displayio.I2CDisplay(display_i2c, device_address=0x3c)
+        self.DISPLAY_WIDTH = 130
+        self.DISPLAY_HEIGHT = 64
+        self.display = adafruit_displayio_sh1106.SH1106(display_bus, width=self.DISPLAY_WIDTH, height=self.DISPLAY_HEIGHT)
+
+        # Display context
+        self.screen = displayio.Group()
+        self.display.show(self.screen)
+        color_bitmap = displayio.Bitmap(self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT, 1)
+        color_palette = displayio.Palette(1)
+        color_palette[0] = 0x000000
+        bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+        self.screen.append(bg_sprite)
+
+        # Text
+        self.label_area = label.Label(terminalio.FONT, text=" "*20, color=0xFFFFFF, x=5, y=5)
+        self.value_area = label.Label(terminalio.FONT, text=" "*20, color=0xFFFFFF, x=48, y=32)
+        self.menu_left_area = label.Label(terminalio.FONT, text="<", color=0xFFFFFF, x=5, y=55)
+        self.menu_right_area = label.Label(terminalio.FONT, text=">", color=0xFFFFFF, x=123, y=55)
+        self.screen.append(self.label_area)
+        self.screen.append(self.value_area)
+        self.screen.append(self.menu_left_area)
+        self.screen.append(self.menu_right_area)
+
+        # Outputs
+        self.FAN = pwmio.PWMOut(board.GP6, frequency=80)
+        self.FAN.duty_cycle = 2 ** 15
+        self.HEAT = pwmio.PWMOut(board.GP8)
+        self.HEAT.duty_cycle = 2 ** 15
+
+        # Menu
+        self.screens = ["dashboard", "temp_set", "timer_set"]
+        self.screen_index = 0
+        self.edit_mode = False
+        self.display_screen(self.screen_index)
+
+    def encoder_handler(self):
+        if self.encoder_last_position is None or self.encoder_last_position != self.encoder.position:
+            if self.encoder.position > self.encoder_last_position:
+                self.edit_handler(-0.5) if self.edit_mode else self.menu_handler(-0.5)
+            else:
+                self.edit_handler(0.5) if self.edit_mode else self.menu_handler(0.5)
+        self.encoder_last_position = self.encoder.position
+
+    def button_handler(self):
+        if self.BTN.value is False and not self.btn_down:
+            self.switch_edit_mode()
+            self.btn_down = True
+        elif self.BTN.value is True and self.btn_down:
+            self.btn_down = False
+
+    def menu_handler(self, increment):
+        self.screen_index += int(increment * 2)
+        if self.screen_index > len(self.screens) - 1:
+            self.screen_index = 0
+        elif self.screen_index < 0:
+            self.screen_index = len(self.screens) - 1
+        self.display_screen(self.screen_index)
+
+    def switch_edit_mode(self):
+        screen = self.screens[self.screen_index]
+        if screen == "temp_set" or screen == "timer_set":
+            if self.edit_mode == False:
+                self.value_area.color=0x000000
+                self.value_area.background_color=0xFFFFFF
+                self.edit_mode = True
+            else:
+                self.value_area.color=0xFFFFFF
+                self.value_area.background_color=0x000000
+                self.edit_mode = False
+                if self.TIME_TIMER_HOURS > 0:
+                    self.TIMER_ON = True
+                    fermenter.timer()
+                else:
+                    self.TIMER_ON = False
+                self.goto("dashboard", "Set!")
+
+    def edit_handler(self, increment):
+        if self.screens[self.screen_index] == "temp_set":
+            self.update_temp_values(increment)
+            self.value_area.text = "{} C".format(round_down(self.TEMP_SET, 1))
+        elif self.screens[self.screen_index] == "timer_set":
+            self.TIME_TIMER_HOURS += increment
+            self.value_area.text = "{} H".format(round_down(self.TIME_TIMER_HOURS, 1))
+
+    def display_screen(self, i):
+        if self.screens[i] == "dashboard":
+            self.label_area.text = "Fermenter"
+            self.update_temp
+            self.value_area.text = "{} C".format(round_down(self.sensor.temperature, 1))
+        elif self.screens[i] == "temp_set":
+            self.label_area.text = "Set: Temperature"
+            self.value_area.text = "{} C".format(round_down(self.TEMP_SET, 1))
+        elif self.screens[i] == "timer_set":
+            self.label_area.text = "Set: Timer"
+            self.value_area.text = "{} H".format(round_down(self.TIME_TIMER_HOURS, 1))
+
+    def update_temp(self, temp):
+        if self.screens[self.screen_index] == "dashboard":
+            self.value_area.text = "{} C".format(round_down(temp, 1))
+
+    def goto(self, screen, message):
+        self.screen_index = self.screens.index(screen)
+        self.value_area.text = message
+        time.sleep(1)
+        self.display_screen(self.screen_index)
+    
+    def heating_system(self, temp):
+        temp_error = abs(self.TEMP_SET - temp)
+        temp_power = simpleio.map_range(temp_error, 0, 5, 0, 100)
+        if temp < self.TEMP_MIN:
+            self.HEAT.duty_cycle = percent_to_duty_cycles(temp_power)
+            self.LED.color = self.COLOR_RED
+            self.STATUS = 1
+            self.FAN.duty_cycle = percent_to_duty_cycles(temp_power*0.6)
+            print('Fermenter heating up. Current: ' + str(temp))
+        elif temp > self.TEMP_MAX:
+            self.LED.color = self.COLOR_BLUE
+            self.STATUS = -1
+            self.FAN.duty_cycle = percent_to_duty_cycles(temp_power)
+            self.HEAT.duty_cycle = 0
+            print('Fermenter cooling down. Current: ' + str(temp))
+        else:
+            self.LED.color = self.COLOR_GREEN
+            self.HEAT.duty_cycle = 0
+            self.STATUS = 0
+            self.FAN.duty_cycle = 0
+            print('Fermenter at the desired temperature. Current: ' + str(temp))
+        self.update_temp(temp)
+
+    def update_temp_values(self, increment):
+        self.TEMP_SET += increment
+        self.TEMP_MAX = self.TEMP_SET + self.TEMP_MARGIN
+        self.TEMP_MIN = self.TEMP_SET - self.TEMP_MARGIN
+
+    def timer(self):
+        time_now = time.time()
+        time_active_sec = time_now - self.TIME_STARTUP
+        time_active_hours = time_active_sec // 3600
+        if self.TIMER_ON:
+            pass
+            # TODO: TIMER
 
 
-# Outputs
-FAN = DigitalInOut(board.GP6)
-FAN.direction = Direction.OUTPUT
-HEAT = DigitalInOut(board.GP7)
-HEAT.direction = Direction.OUTPUT
 
-def menu_handler(pos):
-    global current_screen
-    if current_screen == "temp_display" and pos == "next": next_screen = "temp_set"
-    if current_screen == "temp_display" and pos == "prev": next_screen = "timer_set"
-    if current_screen == "temp_set" and pos == "next": next_screen = "timer_set"
-    if current_screen == "temp_set" and pos == "prev": next_screen = "temp_display"
-    if current_screen == "timer_set" and pos == "next": next_screen = "temp_display"
-    if current_screen == "timer_set" and pos == "prev": next_screen = "temp_set"
-    current_screen = next_screen
-    display_screen(current_screen)
+def percent_to_duty_cycles(percent):
+    duty_cycles = int(simpleio.map_range(percent, 0, 100, 0, 65532))
+    return duty_cycles
 
 def round_down(n, decimals=0):
     multiplier = 10 ** decimals
-    return math.floor(n * multiplier) / multiplier
-
-def display_screen(screen):
-    if screen == "temp_display":
-        label_area.text = "Temperature"
-        update_temp(temp)
-    if screen == "temp_set":
-        label_area.text = "Set: Temperature"
-        value_area.text = "{} C".format(round_down(temp_set, 1))
-    if screen == "timer_set":
-        label_area.text = "Set: Timer"
-        value_area.text = "soon"
- 
-def update_temp(temp):
-    tempText = "{} C".format(round_down(temp, 1))
-    value_area.text = tempText
-
-def button_handler(screen):
-    global edit_mode
-    if screen == "temp_set":
-        if edit_mode == False:
-            value_area.color=0x000000
-            value_area.background_color=0xFFFFFF
-            edit_mode = True
-        else:
-            value_area.color=0xFFFFFF
-            value_area.background_color=0x000000
-            edit_mode = False
-
-def edit_handler(pos):
-    global temp_set
-    if current_screen == "temp_set":
-        if pos == "plus": temp_set += 1
-        if pos == "minus": temp_set -= 1
-        update_temp(temp_set)
-
-display_screen(current_screen)
-
-while True:
-    time.sleep(0.25)
+    number = math.floor(n * multiplier) / multiplier
+    result = 0.0 if number < 0 else number
+    return result
+        
+if __name__ == '__main__':
     
-    # Temperature
-    temp = sensor.temperature
-    if current_screen == "temp_display":
-        update_temp(temp)
+    fermenter = fermenter()
 
-    # Button
-    if BTN.value is False and not btn_down:
-        button_handler(current_screen)
-        btn_down = True
-    if BTN.value is True and btn_down:
-        btn_down = False
-
-    # Encoder
-    position = encoder.position
-    if last_position is None or position != last_position:
-        if position > last_position:
-            edit_handler('minus') if edit_mode else menu_handler('prev')
-        else:
-            edit_handler('plus') if edit_mode else menu_handler('next')
-    last_position = position
-
-    # Actions
-    if temp < temp_set - temp_margin:
-        led.color = (30, 0, 0)
-        HEAT.value = True
-        if fan_freq < fan_freq_max:
-            FAN.value = True
-        elif fan_freq_max <= fan_freq < fan_freq_max*5:
-            FAN.value = False
-        else:
-            fan_freq = 0
-        fan_freq += 1
-        print(fan_freq)
-        print('Fermenter is heating up')
-    elif temp > temp_set + temp_margin:
-        led.color = (0, 0, 30)
-        HEAT.value = False
-        FAN.value = True
-        print('Fermenter is cooling down')
-    else:
-        led.color = (0, 30, 0)
-        HEAT.value = False
-        FAN.value = False
-        print('Fermenter is at the desired temperature')
+    while True:
+        temp = fermenter.sensor.temperature
+        fermenter.encoder_handler()
+        fermenter.button_handler()
+        fermenter.heating_system(temp)
+        # time.sleep(0.25)
