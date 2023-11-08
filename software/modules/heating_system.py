0d3a2edbf3ae0fb826e5dac589dbf5dd720804c8
Antoine Jaunard <antoine.stuff@pm.me>
2023-11-08 17:30:42 +0100

    new power max

--- software/modules/heating_system.py
+++ software/modules/heating_system.py
@@ -102,9 +102,9 @@ def air_circulation(time_diff, interval, duration):
 def heating_system(temp, temp_target, temp_margin):
 
     if FERMENTER_MODEL == "lab":
-        POWER_HEATER = 85  # between 60 and 100
+        POWER_HEATER = 100  # between 60 and 100
     elif FERMENTER_MODEL == "mini":
-        POWER_HEATER = 75  # between 50 and 75
+        POWER_HEATER = 85  # between 50 and 75
 
     temp_error = abs(temp_target - temp)
     power_fan = simpleio.map_range(temp_error, 0, 8, 50, 100)
